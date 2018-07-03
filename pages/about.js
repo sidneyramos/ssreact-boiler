08ceb12c369b4f274fb90a352d491e7b635c7b3a
sidneyramos <sidney.ramos95@gmail.com>
2018-07-03 22:51:45 +1000

    Added next.js

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header'
+import Layout from '../components/Layout'
+
+const About = () => (
+  <Layout>
+    <h3>This is an about page</h3>
+  </Layout>
+)
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-)
+import Link from 'next/link'
+import Layout from '../components/Layout.js'
+import PostLink from '../components/PostLink.js'
 
-export default Index
+function getPosts () {
+  return [
+    { id: 'hello-nextjs', title: 'Hello Next.js'},
+    { id: 'learn-nextjs', title: 'Learn Next.js is awesome'},
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT'},
+  ]
+}
+
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      {getPosts().map((post) => (
+        <PostLink key={post.id} post={post}/>
+      ))}
+    </ul>
+    <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </Layout>
+)
