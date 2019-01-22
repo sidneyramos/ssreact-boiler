e8f5d75d0fb0a326a9fd72fb771b20c6d0286cbf
sidneyramos <sidney.ramos95@gmail.com>
2019-01-23 07:53:10 +1100

    Added graphcms integration

--- components/PostCollection.js
+++ components/PostCollection.js
@@ -1,4 +1,7 @@
 import Link from 'next/link'
+import { Component, Fragment } from 'react'
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 import {
   Container,
   Card,
@@ -13,24 +16,48 @@ import {
 } from 'reactstrap';
 import Header from './Header'
 
-const PostCollection = (props) => (
-  <div className="post-collection">
-    <Row>
-    {props.posts.map((post) => (
-      <Col md={4} sm={12}>
-        <Link href={`/p/${post.id}`}>
-          <a>
-            <Card style={{backgroundImage: `url('${post.image}')`}}>
-              <CardBody>
-                <CardTitle>{post.title}</CardTitle>
-              </CardBody>
-            </Card>
-          </a>
-        </Link>
-      </Col>
-    ))}
-    </Row>
-  </div>
-)
+export const posts = gql`
+  query getPosts {
+    posts {
+      title
+      image
+    }
+  }
+`
 
-export default PostCollection
+class PostCollection extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    console.log(this.props.data);
+    const { props } = this;
+
+    return (
+      <div className="post-collection">
+        <Row>
+          {/*props.posts.map((post) => (
+            <Col md={4} sm={12}>
+              <Link href={`/p/${post.id}`}>
+                <a>
+                  <Card style={{backgroundImage: `url('${post.image}')`}}>
+                    <CardBody>
+                      <CardTitle>{post.title}</CardTitle>
+                    </CardBody>
+                  </Card>
+                </a>
+              </Link>
+            </Col>
+          ))*/}
+          <Col md={4} sm={12}>
+            <h2>Sample</h2>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default graphql(posts)(PostCollection)
